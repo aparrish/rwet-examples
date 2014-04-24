992f471be82a4c39d1e73dfaccf9ce89b0105589
Adam Parrish <adam@decontextualize.com>
2014-04-24 12:30:36 -0400

    modified tornado examples

--- tornado/markovtweets/markovtweets.py
+++ tornado/markovtweets/markovtweets.py
@@ -1,30 +1,23 @@
-import json
-import urllib
-import logging
-import os.path
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
+import os
 
-from tornado.options import define, options
+import twython
 
 from markov_by_char import CharacterMarkovGenerator
 
+from tornado.options import define, options
+
 define("port", default=8888, help="run on the given port", type=int)
 define("n", default=6, help="length of n-gram", type=int)
+define("api_key", help="twitter api key")
+define("api_secret", help="twitter api secret")
+define("access_token", help="twitter access token")
+define("token_secret", help="twitter token secret")
 
-class Application(tornado.web.Application):
-	def __init__(self):
-		handlers = [
-			(r"/", IndexHandler),
-			(r"/markov", MarkovHandler)
-		]
-		settings = dict(
-			template_path=os.path.join(os.path.dirname(__file__), "templates"),
-			static_path=os.path.join(os.path.dirname(__file__), "static")
-		)
-		tornado.web.Application.__init__(self, handlers, **settings)
+tornado.options.parse_command_line()
 
 class IndexHandler(tornado.web.RequestHandler):
 	def get(self):
@@ -32,21 +25,24 @@ class IndexHandler(tornado.web.RequestHandler):
 
 class MarkovHandler(tornado.web.RequestHandler):
 	def get(self):
-		screen_name = self.get_argument('screen_name')
-		params = {'count': 200, 'screen_name': screen_name}
-		# fetch tweets
-		resp = urllib.urlopen(
-			'http://api.twitter.com/1/statuses/user_timeline.json?' + \
-			urllib.urlencode(params))
-		rawjson = resp.read()
+		twitter = twython.Twython(options.api_key, options.api_secret,
+				options.access_token, options.token_secret)
+		response = twitter.get_user_timeline(
+				screen_name=self.get_argument('screen_name'),
+				count=200)
 		generator = CharacterMarkovGenerator(options.n, 140)
-		data = json.loads(rawjson)
-		for tweet in data:
+		for tweet in response:
 			generator.feed(tweet['text'])
 		self.write(generator.generate())
 
-if __name__ == "__main__":
-	tornado.options.parse_command_line()
-	http_server = tornado.httpserver.HTTPServer(Application())
-	http_server.listen(options.port)
-	tornado.ioloop.IOLoop.instance().start()
+static_path = os.path.join(os.path.dirname(__file__), "static")
+template_path = os.path.join(os.path.dirname(__file__), "templates")
+application = tornado.web.Application(
+		handlers=[(r"/", IndexHandler), (r"/markov", MarkovHandler)],
+		static_path=static_path,
+		template_path=template_path)
+
+http_server = tornado.httpserver.HTTPServer(application)
+http_server.listen(options.port)
+tornado.ioloop.IOLoop.instance().start()
+
